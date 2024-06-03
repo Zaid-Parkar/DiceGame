1389c124fde9c16eac1cc13ae4ced609289979f2
Zaid Parkar <itsme.zaidparkar@gmail.com>
2024-06-03 19:42:34 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+   backgroud-color : black;
+   color : white;
+   padding : 10px;
+`; 
+
+
+const App = () => {
+  return (
+    <>
+  <Button>hello</Button>
+    </>
+  )
+}
+
+export default App
